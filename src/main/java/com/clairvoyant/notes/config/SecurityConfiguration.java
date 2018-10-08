516d2e50f0781dcfb44ec4ab61b0c2a6d8a3622a
Shoaib Shaikh <shoaib.shoaib@clairvoyantsoft.com>
2018-10-08 20:35:18 +0530

    configure For CORS

--- src/main/java/com/clairvoyant/notes/config/SecurityConfiguration.java
+++ src/main/java/com/clairvoyant/notes/config/SecurityConfiguration.java
@@ -5,9 +5,13 @@ import com.clairvoyant.notes.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -26,7 +30,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
         auth.userDetailsService(userDetailsService).passwordEncoder(getPasswordEncoder());
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
 
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("**/secured/**").authenticated()
+                .anyRequest().permitAll()
+//                .antMatchers("/auth/login", "/health", "/metrics").permitAll()
+                .and()
+                .formLogin().permitAll();
+    }
 
     private PasswordEncoder getPasswordEncoder() {
         return new PasswordEncoder() {
